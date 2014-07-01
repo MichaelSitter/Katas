5ba10d48b1268b4e70554d9668be5e8f28ee3a62
Michael S <msi@tradeshift.com>
2014-07-01 10:14:28 -0700

    Passing II -> 2

--- RomanNumerals/src/calc.js
+++ RomanNumerals/src/calc.js
@@ -1,11 +1,21 @@
 function Calculator(){
-	this.convert = function (input) {
-		if (input == 'X') {
+
+	var getValue = function (char) {
+		if (char == 'X') {
 			return 10;
 		}
-		if (input == 'V') {
+		if (char == 'V') {
 			return 5;
 		}
 		return 1;
+	};
+
+	this.convert = function (input) {
+
+		var total = 0;
+		for (var i = 0; i < input.length; i++) {
+			total += getValue(input[i]);
+		};
+		return total;
 	}
 };

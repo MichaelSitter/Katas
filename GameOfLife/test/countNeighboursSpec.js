b83c9001658498eb97efe15e08a9eb1f6d2fe632
Michael S <msi@tradeshift.com>
2014-07-01 14:28:34 -0700

    Failing case for neighbour checking at world boundry

--- GameOfLife/test/countNeighboursSpec.js
+++ GameOfLife/test/countNeighboursSpec.js
@@ -4,14 +4,16 @@ describe('countNeighboursSpec', function() {
 
 	describe('Given empty 3 x 3 world', function () {
 
-		world = [
-			[0,0,0],
-			[0,0,0],
-			[0,0,0]
-		];
+		beforeEach(function () {
+			world = [
+				[0,0,0],
+				[0,0,0],
+				[0,0,0]
+			];
+		});
 
 		describe('And given checking cell 1,1', function () {
-			x = 1, y = 1;
+			beforeEach(function () { x = 1, y = 1; });
 
 			it('Should have no neighbours', function () {
 				expect(countNeighbours(world, x, y)).toEqual(0);
@@ -19,6 +21,40 @@ describe('countNeighboursSpec', function() {
 		});
 	});
 
+	describe('Given 3 x 3 world with row of live cells', function () {
+
+		beforeEach(function () {
+			world = [
+				[1,1,1],
+				[0,0,0],
+				[0,0,0]
+			];
+		});
+
+		describe('And given checking cell 1,1', function () {
+			beforeEach(function () { x = 1, y = 1; });
+
+			it('Should have 3 neighbours', function () {
+				expect(countNeighbours(world, x, y)).toEqual(3);
+			});
+		});
+	});
+
+	describe('Given 1 x 1 world', function () {
+
+		beforeEach(function () {
+			world = [[0]];
+		});
+
+		describe('And given checking cell in bounds', function () {
+			beforeEach(function () { x = 0, y = 0; });
+
+			// Cells outside the world are dead 
+			it('Should have no neighbours', function () {
+				expect(countNeighbours(world, x, y)).toEqual(0);
+			});
+		});
+	});
 
 
 });
